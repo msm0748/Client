553d4ea8efdacb28b91dae91560b272c2dd18085
seokmin <msm0748@gmail.com>
2023-12-12 07:01:12 +0900

    refactor: 레이아웃 구조 변경

--- src/app/(baseLayout)/layout.tsx
+++ src/app/(baseLayout)/layout.tsx
@@ -1,4 +1,4 @@
-import Layout from '@/components/layout';
+import Layout from './_component';
 
 interface Props {
   children: React.ReactNode;
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Layout from '@/components/layout';
-
-export default function Home() {
-  return <Layout>메인 페이지</Layout>;
-}
