5145d880d5c56d2b573aface10bdc3eff2a445ee
seokmin <msm0748@gmail.com>
2023-12-13 14:25:01 +0900

    design: 카테고리 사이즈 조정

--- src/app/(baseLayout)/_component/Header/Category.tsx
+++ src/app/(baseLayout)/_component/Header/Category.tsx
@@ -11,14 +11,11 @@ export default function Category() {
       <ul className="flex items-center justify-between font-semibold overflow-x-auto flex-nowrap no-scroll">
         {category.map((item, index) => (
           <li key={index} className="relative">
-            <Link
-              href={item.href}
-              className="block py-3 md:px-2 px-3 md:text-lg whitespace-nowrap"
-            >
+            <Link href={item.href} className="block md:py-3 py-1.5 md:px-2 px-3 md:text-lg whitespace-nowrap">
               {item.text}
             </Link>
             {item.segment === segment && (
-              <span className="block md:w-full w-[calc(100%-1rem)] mx-auto h-1 bg-black mt-3 absolute bottom-0 right-0 left-0"></span>
+              <span className="block md:w-full w-[calc(100%-1rem)] mx-auto h-0.5 bg-black mt-3 absolute bottom-0 right-0 left-0"></span>
             )}
           </li>
         ))}
