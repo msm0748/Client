00d56907cd2137c561dfb9685d72f756c36cf913
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-12-15 03:43:28 +0900

    feat: 버튼에 링크 추가

--- src/app/(baseLayout)/_component/Footer/MobileMenubar.tsx
+++ src/app/(baseLayout)/_component/Footer/MobileMenubar.tsx
@@ -13,19 +13,20 @@ export default function MobileMenubar() {
       <nav>
         <ul className="flex justify-around bg-primary-200 items-center pt-2.5 pb-1.5">
           <li>
-            <button className="flex flex-col gap-1 items-center justify-center text-xl">
+            <button type="button" className="flex flex-col gap-1 items-center justify-center text-xl">
               <IoMdSearch />
               <span className="text-xs text-neutral-500">검색</span>
             </button>
           </li>
           <li>
-            <button className="flex flex-col gap-1 items-center justify-center text-xl">
+            <button type="button" className="flex flex-col gap-1 items-center justify-center text-xl">
               {segment === 'category' ? <BsGridFill /> : <BsGrid />}
               <span className="text-xs text-neutral-500">카테고리</span>
             </button>
           </li>
           <li>
             <button
+              type="button"
               onClick={() => router.push('/')}
               className="flex flex-col gap-1 items-center justify-center text-xl"
             >
@@ -34,13 +35,21 @@ export default function MobileMenubar() {
             </button>
           </li>
           <li>
-            <button className="flex flex-col gap-1 items-center justify-center text-xl">
-              {segment === 'favorite' ? <IoMdHeart /> : <IoMdHeartEmpty />}
+            <button
+              type="button"
+              onClick={() => router.push('/favor')}
+              className="flex flex-col gap-1 items-center justify-center text-xl"
+            >
+              {segment === 'favor' ? <IoMdHeart /> : <IoMdHeartEmpty />}
               <span className="text-xs text-neutral-500">찜</span>
             </button>
           </li>
           <li>
-            <button className="flex flex-col gap-1 items-center justify-center text-xl">
+            <button
+              type="button"
+              onClick={() => router.push('/mypage')}
+              className="flex flex-col gap-1 items-center justify-center text-xl"
+            >
               {segment === 'mypage' ? <BsPersonFill /> : <BsPerson />}
               <span className="text-xs text-neutral-500">마이</span>
             </button>
