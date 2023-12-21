fc280f2a08408c6758f43e3e1716d1088a987085
seokmin <msm0748@gmail.com>
2023-12-22 06:07:35 +0900

    refactor: next-auth 버전 변경으로 인한 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,5 @@
-import { auth } from './auth';
-import { NextResponse } from 'next/server';
+export { default } from 'next-auth/middleware';
 
-export async function middleware() {
-  const session = await auth();
-  if (!session) {
-    return NextResponse.redirect('http://localhost:3000/login');
-  }
-}
-
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: ['/mypage'],
 };
