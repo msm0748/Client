fd4909bd62976d92d3dc0be8e84a776260e3c529
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-12-23 07:32:16 +0900

    feat: 관리자 기능 페이지 추가

---
+++ src/app/admin/stats/page.tsx
@@ -0,0 +1,11 @@
+import Header from '../_component/Header';
+
+export default function StatsPage() {
+  return (
+    <div>
+      <Header>
+        <h2 className="admin-h2">통계</h2>
+      </Header>
+    </div>
+  );
+}
