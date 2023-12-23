2994f857711c41f966023b1f5017263942c42ef3
seokmin <msm0748@gmail.com>
2023-12-23 19:21:01 +0900

    refactor: 관리자 쇼핑 페이지 수정

--- src/app/admin/shopping/page.tsx
+++ src/app/admin/shopping/page.tsx
@@ -1,11 +1,6 @@
-import Header from '../_component/Header';
-
 export default function ShoppingPage() {
   return (
     <div>
-      <Header>
-        <h2 className="admin-h2">쇼핑</h2>
-      </Header>
       <div>쇼핑</div>
     </div>
   );
