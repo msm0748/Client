f7f2a9fee82e164c62fb4924a14c2813f841c831
seokmin <msm0748@gmail.com>
2023-12-23 20:14:19 +0900

    feat: 관리자 페이지 header 추가

--- src/app/admin/stats/page.tsx
+++ src/app/admin/stats/page.tsx
@@ -1,5 +1,11 @@
 import Header from '../_component/Header';
 
 export default function StatsPage() {
-  return <div>통계</div>;
+  return (
+    <div>
+      <Header>
+        <h2 className="admin-h2">통계</h2>
+      </Header>
+    </div>
+  );
 }
--- src/app/admin/stats/take/page.tsx
+++ src/app/admin/stats/take/page.tsx
@@ -1,12 +1,11 @@
 import Header from '../../_component/Header';
 
-export default function OrderPage() {
+export default function TakePage() {
   return (
     <div>
       <Header>
-        <h2 className="admin-h2">주문</h2>
+        <h2 className="admin-h2">매출</h2>
       </Header>
-      <div>주문</div>
     </div>
   );
 }
