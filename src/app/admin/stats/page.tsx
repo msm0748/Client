426b4f3960fa11a5c50a516899b5f9ca7b5baeb3
seokmin <msm0748@gmail.com>
2023-12-23 19:20:06 +0900

    refactor: 관리자 통계 페이지 수정

--- src/app/admin/stats/page.tsx
+++ src/app/admin/stats/page.tsx
@@ -1,11 +1,5 @@
 import Header from '../_component/Header';
 
 export default function StatsPage() {
-  return (
-    <div>
-      <Header>
-        <h2 className="admin-h2">통계</h2>
-      </Header>
-    </div>
-  );
+  return <div>통계</div>;
 }
