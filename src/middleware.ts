2990b30058c0b85aaa31aa01ad3509e6af84dbbb
seokmin <msm0748@gmail.com>
2023-12-27 01:52:12 +0900

    feat: 관리자 페이지 접근 권한 설정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,22 @@
-export { default } from 'next-auth/middleware';
+import { auth } from './auth';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  const session = await auth();
+
+  if (request.nextUrl.pathname.startsWith('/mypage')) {
+    if (!session) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }
+
+  if (request.nextUrl.pathname.startsWith('/admin')) {
+    if (session?.role !== 'admin') {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
+}
 
 export const config = {
-  matcher: ['/mypage'],
+  matcher: ['/mypage/:path*', '/admin/:path*'],
 };
