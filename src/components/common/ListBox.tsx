e38b9bd428da453aa7791ab0892e0ccfaa3453b3
seokmin <msm0748@gmail.com>
2024-01-03 17:31:07 +0900

    design: 텍스트 사이즈 수정

--- src/components/common/ListBox.tsx
+++ src/components/common/ListBox.tsx
@@ -15,14 +15,14 @@ export default function ListBox({ data, selected, setSelected }: Props) {
     <div className="w-full">
       <HeadlessListBox value={selected} onChange={setSelected}>
         <div className="relative mt-1">
-          <HeadlessListBox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+          <HeadlessListBox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 text-sm">
             <span className="block truncate">{selected.name}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
             </span>
           </HeadlessListBox.Button>
           <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
-            <HeadlessListBox.Options className="z-10 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+            <HeadlessListBox.Options className="z-10 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none text-sm">
               {data.map((item) => (
                 <HeadlessListBox.Option
                   key={item.id}
