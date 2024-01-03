a4d5c0b10f4654a7a42499b57c3f9798cbbcac04
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2024-01-04 03:20:14 +0900

    refactor: 쿼리 파라미터를 이용한 페이지네이션으로 수정

--- src/app/(baseLayout)/_component/ui/ProductList.tsx
+++ src/app/(baseLayout)/_component/ui/ProductList.tsx
@@ -1,35 +1,39 @@
 'use client';
+
 import Card from '@/components/common/Card';
 import CardList from '@/components/common/CardList';
-import { useEffect, useState } from 'react';
 import ListBox from '@/components/common/ListBox';
 import useCategoryFilter from '@/hooks/useCategoryFilter';
 import { sortingOptions } from '@/data/sortingOptions';
 import Pagination from './Pagination';
-import { Product } from '@/types/product';
+import { Product } from '@/types/Product';
+import { useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 interface Props {
   products: Product[];
 }
 
 export default function ProductList({ products }: Props) {
-  const { data, categorySelected, setCategorySelected } = useCategoryFilter(products, sortingOptions);
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 20;
-  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const { data, categorySelected, setCategorySelected } = useCategoryFilter(products);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const page = searchParams.get('page');
+  const currentPage = Number(page) || 1;
 
-  // 현재 페이지에 필요한 데이터 추출
+  const itemsPerPage = 20;
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const currentPageData = data.slice(startIndex, endIndex);
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
+  const totalPages = Math.ceil(data.length / itemsPerPage);
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, [currentPage]);
+
+  const handlePageChange = (pageIndex: number) => {
+    router.push(`?page=${pageIndex}`);
+  };
 
   return (
     <main className="container">
