7dfeb24ff977f1b847d76d93ff6c4f8dd77ebca0
seokmin <msm0748@gmail.com>
2024-01-03 18:17:32 +0900

    feat: sortingOptions 데이터 생성

---
+++ src/data/sortingOptions.ts
@@ -0,0 +1,18 @@
+export const sortingOptions = [
+  {
+    id: 1,
+    name: '인기순',
+  },
+  {
+    id: 2,
+    name: '구매후기 많은순',
+  },
+  {
+    id: 3,
+    name: '낮은 가격순',
+  },
+  {
+    id: 4,
+    name: '높은 가격순',
+  },
+];
