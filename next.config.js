08cc10ba1ef9b0af8dfa5c99664b2c8a033ed5d5
seokmin <msm0748@gmail.com>
2024-01-03 15:51:54 +0900

    refactor: Next.js image 경로 변경

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'tailwindui.com',
+        hostname: 'image.idus.com',
         pathname: '**',
       },
     ],
