e2b8493bb8a3de81d1c3be385cb008e72a010130
seokmin <msm0748@gmail.com>
2024-01-04 21:55:01 +0900

    refactor: 더미 데이터 폴더로 분리

---
+++ src/data/dummy/product/toy.ts
@@ -0,0 +1,38 @@
+export const toy = [
+  {
+    id: 100,
+    name: '람찌네소품샵 캐릭터 여권케이스 다람쥐 돼지 호랑이',
+    image:
+      'https://image.idus.com/image/files/9bc49908510e4ed3b476107cec9962c0_400.jpg',
+    price: 18000,
+    rating: 90,
+    reviewCount: 32,
+  },
+  {
+    id: 101,
+    name: '헤어샵 미용실 미니어처 명함꽂이',
+    image:
+      'https://image.idus.com/image/files/0856c3f3848442c3a9f0ecaf72014c73_400.jpg',
+    price: 49000,
+    rating: 80,
+    reviewCount: 24,
+  },
+  {
+    id: 102,
+    name: '람찌네소품샵 펜 파우치 캐릭터 필통',
+    image:
+      'https://image.idus.com/image/files/cbb14458563f4eb88007783f34d9744c_400.png',
+    price: 12600,
+    rating: 80,
+    reviewCount: 422,
+  },
+  {
+    id: 103,
+    name: '특별한개업선물💅네일샵개업선물',
+    image:
+      'https://image.idus.com/image/files/3c9fc24c59b74dc1b6a62a87e3f5ef3a_400.jpg',
+    price: 65000,
+    rating: 78,
+    reviewCount: 142,
+  },
+];
