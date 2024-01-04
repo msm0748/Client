22d2b720b2abf75ac95adc96e97e830ef2762995
seokmin <msm0748@gmail.com>
2024-01-05 03:51:28 +0900

    refactor: 카테고리 active 방법 수정

--- src/app/(baseLayout)/_component/layout/Header/Category.tsx
+++ src/app/(baseLayout)/_component/layout/Header/Category.tsx
@@ -2,10 +2,10 @@
 
 import { category } from '@/data/category';
 import Link from 'next/link';
-import { useSelectedLayoutSegment } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 export default function Category() {
-  const segment = useSelectedLayoutSegment();
+  const pathname = usePathname();
 
   return (
     <nav className="px-2 md:px-0">
@@ -15,7 +15,7 @@ export default function Category() {
             <Link href={item.href} className="block md:py-3 py-1.5 md:px-2 px-3 md:text-lg whitespace-nowrap">
               {item.text}
             </Link>
-            {item.segment === segment && (
+            {item.href === pathname && (
               <span className="block md:w-full w-[calc(100%-1rem)] mx-auto h-0.5 bg-black mt-3 absolute bottom-0 right-0 left-0"></span>
             )}
           </li>
