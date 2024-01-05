a4ed01d9fcd69fdd970a781bd0267f99ba9752bc
seokmin <msm0748@gmail.com>
2024-01-05 10:04:18 +0900

    design: 상품 상세 페이지 총 상품 금액 색상 변경

--- src/app/products/[productId]/_component/ProductInfo/TotalPrice.tsx
+++ src/app/products/[productId]/_component/ProductInfo/TotalPrice.tsx
@@ -7,7 +7,7 @@ export default function TotalPrice({ sale, count }: Props) {
   return (
     <div className="flex justify-between items-end">
       <span className="md:text-lg text-sm">총 상품 금액</span>
-      <div className="text-orange-700">
+      <div className="text-red-600">
         <span className="font-bold md:text-3xl text-lg">
           {(sale * count >= 50000 ? sale * count : sale * count + 3500).toLocaleString('ko-KR')}
         </span>
